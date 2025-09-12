605e6a007b9db0a5cd20069570692518e0f53ae6
youngrok <yr-park@kaist.ac.kr>
2025-09-12 23:48:26 +0900

    weight

--- custom_generate/generate.py
+++ custom_generate/generate.py
@@ -279,7 +279,7 @@ def generate(
                 ess = 1.0 / torch.sum(norm_weights**2, dim=-1)
                 needs_resampling_mask = initial_needs_resampling_mask.clone()
                 needs_resampling_mask[initial_needs_resampling_mask] &= (ess < unfinished_reshaped.sum(-1)[initial_needs_resampling_mask] * smc_resample_threshold)
-                breakpoint()
+
                 if needs_resampling_mask.any():
                     norm_weights = norm_weights[needs_resampling_mask[initial_needs_resampling_mask]]
                     resampled_local_indices = torch.multinomial(
